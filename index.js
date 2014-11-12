57eddeff52a76e313555f7b6bd8e476fa183b388
B3rn475 <b3rn4@hotmail.it>
2014-11-12 14:45:06 +0100

    Beutified code

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /*jslint node: true, nomen: true, es5: true */
 /** 
- * Developed By Carlo Bernaschina (GitHub - B3rn475) 
- * www.bernaschina.com 
- * 
- * Distributed under the MIT Licence 
+ * Developed By Carlo Bernaschina (GitHub - B3rn475)
+ * www.bernaschina.com
+ *
+ * Distributed under the MIT Licence
  */
 "use strict";
 
